735d73226ad34bcfdc66f844e7e066dbeaabf0f0
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-04-23 14:23:16 +0100

    is the report function ignored?

---
+++ fix-example/remove-chazam.js
@@ -0,0 +1,15 @@
+module.exports.report = () =>'Unexpected "debugger" statement';
+
+module.exports.find = (ast, {traverse}) => {
+    const places = [];
+    traverse(ast, {
+        DebuggerStatement(path) {
+            places.push(path);
+        }
+    });
+    return places;
+};
+
+module.exports.fix = (path) => {
+    path.remove();
+};
--- input-for-remove-debugger.js
+++ input-for-remove-debugger.js
@@ -1,2 +1,3 @@
 console.log("hello");
+debugger;
 console.error("hello");
