46c8d5e573d456e57f14ab8cce9121835fbe673e
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-04-24 13:43:14 +0100

    usepo.mjs

--- usepo.mjs
+++ usepo.mjs
@@ -1,12 +1,17 @@
 #!/usr/bin/env node
 import putout from 'putout';
 import fs from 'fs';
-const source = fs.readFileSync(process.argv[2], 'utf8');
-
+const inputFilename = process.argv[2] || 'input-for-my-transform.js';
+const source = fs.readFileSync(inputFilename, 'utf8');
+console.log(`----${inputFilename}----\n${source}\n`);
+const transfomName = process.argv[3] || 'my-transform';
+transfomName.replace(/^trasforms\/putout-plugin-/, '');
 let result = putout(source, {
     plugins: [
-        'remove-unused-variables',
-        'my-transform', // searches for putout-plugin-my-transform.js
+        //'remove-unused-variables',
+        // 'my-transform', // searches for putout-plugin-my-transform.js
+        // 'rem-unrech-cod',
+        transfomName
     ],
 });
-console.log(result.code);
+console.log(`----Transformed Code----\n${result.code}\n`);
